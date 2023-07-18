ebf68d9ff97fd504072040504ed12d52236bed6c
mdkdinesh2503 <mdkdinesh2503@gmail.com>
2023-07-18 17:31:58 +0530

    Error Display using Exception Handling

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -84,4 +84,14 @@ export class AuthService {
     xlsx.writeFile(workBook, fileName+'.xlsx');
   }
 
+  AlertMessage:String ="";
+
+  errorDisplay(message:any) {
+    this.AlertMessage = message;
+  }
+
+  errorPage() {
+    return this.AlertMessage;
+  }
+
 }
